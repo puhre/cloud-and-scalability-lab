5ff21cdae4812634a086ca14cb617241bf585c11
puhre <jens.rehn@live.com>
2021-02-05 15:52:59 +0100

    Added nore things

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Expressomachino arguh' });
+  res.render('index', { title: 'Expressomachino arguh added some extra\n\thargh' });
 });
 
 module.exports = router;
